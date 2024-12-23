1944dfd67bac00b3e167ff8d8cbd4376e2dc69ea
Ayden Garza <aydenjgarza@gmail.com>
2024-12-23 13:37:49 -0600

    graphiviz is working yay

--- micrograd.py
+++ micrograd.py
@@ -1,11 +1,16 @@
+import numpy as np
+
 
 class Value:
-    def __init__ (self, data, _children = (), _op = ""):
+    def __init__ (self, data, _children = (), _op = "", label=""):
         self.data = data
         self._prev = set(_children)
+        self._op = _op
+        self.label = label
     
     def __add__ (self, other):
-        outVal = Value(self.data + other.data, (self, other), "+")
+        outVal = Value(self.data * other.data, (self, other), "+")
+        print("Llds")
         return outVal
     
     def __mul__ (self, other):
@@ -14,4 +19,8 @@ class Value:
     
     def __repr__(self):
         return f"Value(data={self.data})"
-    
+
+a = Value(2)
+b = Value(3)
+c = a*b
+print(c._op)
