20855679af45a9390be88ff76fcfb72b712fed9e
Ayden Garza <aydenjgarza@gmail.com>
2024-12-23 15:42:02 -0600

    Backprop complete

--- micrograd.py
+++ micrograd.py
@@ -1,5 +1,5 @@
-import numpy as np
-
+import math
+from visualization import draw_dot
 
 class Value:
     def __init__ (self, data, _children = (), _op = "", label=""):
@@ -7,20 +7,53 @@ class Value:
         self._prev = set(_children)
         self._op = _op
         self.label = label
+        self._backward = lambda: None
+        self.grad = 0
     
     def __add__ (self, other):
-        outVal = Value(self.data * other.data, (self, other), "+")
-        print("Llds")
+        outVal = Value(self.data + other.data, (self, other), "+")
+        def _backward():
+            self.grad = outVal.grad
+            other.grad = outVal.grad
+        outVal._backward = _backward
         return outVal
     
     def __mul__ (self, other):
         outVal = Value(self.data * other.data, (self, other), "*")
+        def _backward():
+            self.grad = other.data * outVal.grad
+            other.grad = self.data * outVal.grad
+        outVal._backward = _backward
         return outVal
     
     def __repr__(self):
         return f"Value(data={self.data})"
+    
+    def tanh(self):
+        x = self.data
+        tanhx = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+        outVal = Value(tanhx, (self,), "tanh")
+        def _backward():
+            self.grad = (1-tanhx**2) * outVal.grad
+        outVal._backward = _backward
+        return outVal
+    def backward(self):
+        self.grad = 1
 
-a = Value(2)
-b = Value(3)
-c = a*b
-print(c._op)
+        ts = []
+        visited = set()
+        def build_toposort(n):
+            if n not in visited:
+                visited.add(n)
+                for child in n._prev:
+                    build_toposort(child)
+                ts.append(n)
+        build_toposort(self)
+        ts = reversed(ts)
+        for node in ts:
+            node._backward()
+
+a = Value(-2 ,label = "a")
+b = Value(-3, label = "b")
+c = Value(10, label = "c")
+d = a*b+c
